a8a81e9e6e69c871c684ca6211bca151e17e2bc4
tayyab.centrox <tayyab.centrox@gmail.com>
2026-01-23 00:22:14 +0500

    Updates

---
+++ assets/index-C4JotEj1.js
@@ -0,0 +1,1 @@
+import{j as e}from"./main-sh7plqeD.js";function r(){return e.jsx("div",{className:"min-h-screen flex items-center justify-center",children:e.jsx("h1",{children:"Demo"})})}export{r as component};
